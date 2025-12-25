5c25333c4cebcce7d3fd4759ed53694b438b83e9
Sherrrrryyy <Shaheerkhan090909@gmail.com>
2025-12-25 16:10:20 +0500

    enhance registration form with additional fields and styling

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const userRouter = require("./routes/user.router.js");
 const app = express();
 app.set("view engine", "ejs");
 
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 
 app.use('/user', userRouter);
 
--- routes/user.router.js
+++ routes/user.router.js
@@ -5,11 +5,14 @@ const router = express.Router();
 //   res.send("User route is working");
 // });
 
-
 // ek route jha par form show hoga or dusra jha data jayega
-router.get('/register',(req,res)=>{
-    res.render('register')
-})
+router.get("/register", (req, res) => {
+  res.render("register");
+});
 
+router.post("/register", (req, res) => {
+  console.log(req.body);
+  res.send("Registration successful");
+});
 
 module.exports = router;
