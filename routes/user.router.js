56dc772631914d842ce48fc29f92d301a11aa2d3
Sherrrrryyy <Shaheerkhan090909@gmail.com>
2025-12-25 15:08:18 +0500

    add registration route and create register view

--- routes/user.router.js
+++ routes/user.router.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/test", (req, res) => {
-  res.send("User route is working");
-});
+// router.get("/test", (req, res) => {
+//   res.send("User route is working");
+// });
+
+
+// ek route jha par form show hoga or dusra jha data jayega
+router.get('/register',(req,res)=>{
+    res.render('register')
+})
+
 
 module.exports = router;
