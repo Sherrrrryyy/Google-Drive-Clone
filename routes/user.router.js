dd04f4052896633464ea2cfb20a86229c7fce072
Sherrrrryyy <Shaheerkhan090909@gmail.com>
2025-12-26 16:24:30 +0500

    feat: add JWT authentication to login route and update dependencies

--- routes/user.router.js
+++ routes/user.router.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
-const userModel = require("../model/userModel");
+const jwt = require("jsonwebtoken");
 const router = express.Router();
 const { body, validationResult } = require("express-validator");
+const userModel = require("../model/userModel");
 
 // router.get("/test", (req, res) => {
 //   res.send("User route is working");
@@ -44,12 +45,56 @@ router.post(
   }
 );
 
-
-// Login Html page rendering
+// login html page rendering
 router.get("/login", (req, res) => {
   res.render("login");
 });
 
-// login route
+// Login Html page rendering
+router.post(
+  "/login",
+
+  body("email").trim().isEmail(),
+  body("password").trim().isLength({ min: 6 }),
+
+  async (req, res) => {
+    const error = validationResult(req);
+
+    if (!error.isEmpty()) {
+      return res.status(400).json({
+        errors: error.array(),
+        message: "Email or Password is incorrect",
+      });
+    }
+
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({ email });
+
+    if (!user) {
+      return res
+        .status(400)
+        .json({ message: "Email or Password is incorrect" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res
+        .status(400)
+        .json({ message: "Email or Password is incorrect"`` });
+    }
+
+    const token = jwt.sign(
+      {
+        userId: user._id,
+        email: user.email,
+        username: user.username,
+      },
+      process.env.JWT_SECRET_KEY
+    );
+    res.json({ token });
+  }
+);
 
 module.exports = router;
