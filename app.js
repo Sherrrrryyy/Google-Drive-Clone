30dc2645ebb4f85609e9e05c149ade01884c7fa4
Sherrrrryyy <Shaheerkhan090909@gmail.com>
2025-12-30 16:24:10 +0500

    feat: implement home page with file upload functionality and popup modal

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 const express = require("express");
+const app = express();
 const cookieParser = require("cookie-parser");
 const userRouter = require("./routes/user.router.js");
+const indexRouter = require('./routes/index.route.js')
 const dotenv = require('dotenv')
 dotenv.config();
 const connectToDB = require('./config/db.js')
 connectToDB();
 
-const app = express();
 app.set("view engine", "ejs");
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-
+app.use('/', indexRouter);
 app.use('/user', userRouter);
 
 
